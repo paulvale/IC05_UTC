a763f6f3a002ff3a798ef1d137a44180218267a7
Valentin Paul <valentin.paul@etu.utc.fr>
2016-11-30 10:31:07 +0100

    test with vis.js => Not a good library need to change to another one

--- angularJs/app.js
+++ angularJs/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('ic05App', ['ui.router']);
+var app = angular.module('ic05App', ['ui.router', 'ngVis']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise("/presentation");
--- pages/visualisation/visualisationController.js
+++ pages/visualisation/visualisationController.js
@@ -1,3 +1,38 @@
-app.controller('VisualisationController',function ($scope, $state){
+app.controller('VisualisationController', function ($scope, $state, VisDataSet, $http) {
+    // load the JSON file containing the Gephi network.
+    $http.get('graphTest.json')
+       .then(function(res){
+          $scope.data = res.data;     
+          console.log(res.data)           
+        });
+    
+    $scope.onSelect = function(items) {
+      // debugger;
+      alert('select');
+    };
+
+    $scope.onClick = function(props) {
+      //debugger;
+      alert('Click');
+    };
+
+    $scope.onDoubleClick = function(props) {
+      // debugger;
+      alert('DoubleClick');
+    };
+
+    $scope.rightClick = function(props) {
+      alert('Right click!');
+      props.event.preventDefault();
+    };
+    
+    $scope.options = {
+      autoResize: true,
+      height: '800',
+      width: '100%'
+    };
+
+    console.log("load reussi")
+    //$scope.data = gephiJSON;
 
 });
