3c87c208d574b3aa1a975360d76aa409d6bcc3d3
Valentin Paul <valentin.paul@etu.utc.fr>
2016-12-01 15:21:54 +0100

    test du site

--- pages/annuaire/annuaireController.js
+++ pages/annuaire/annuaireController.js
@@ -1,3 +1,7 @@
-app.controller('AnnuaireController',function ($scope, $state){
-
+app.controller('AnnuaireController', function ($scope, $state, $http) {
+    $http.get('annuaire.json')
+        .then(function (res) {
+            $scope.data = res.data;
+            console.log(res.data)
+        });
 });
